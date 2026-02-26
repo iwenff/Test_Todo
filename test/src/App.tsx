8c88c7c4dd777ff415b3c9d9aefa72de9aecbe84
whqr <whqr@MacBookNikita.local>
2026-02-26 22:17:37 +0300

    commit

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 import { TodoPage } from "./features/todos/ui/TodoPage";
 import { AppLayout } from "./App/Layout/AppLayout";
-import { ThemeProvider } from "./app/providers/ThemeProvider";
+
 import "./App.css";
+import { ThemeProvider } from "./App/Providers/ThemeProvider";
 
 export const App: React.FC = () => {
   return (
--- test/src/shared/ui/ThemeToggle.tsx
+++ test/src/shared/ui/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTheme } from "../../app/providers/ThemeProvider/index";
+import { useTheme } from "../../App/Providers/ThemeProvider";
 
 export const ThemeToggle: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
